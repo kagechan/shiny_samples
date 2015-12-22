234cd13b17e28db11d3e421115ed4cade855ff00
Yoshiteru Kageyama <yt-kage@cb3.so-net.ne.jp>
2015-12-23 00:35:44 +0900

    Added README.md file to explain novice users how to use this shiny application.

--- Developing_Data_Products_assignment/ui.R
+++ Developing_Data_Products_assignment/ui.R
@@ -1,22 +1,22 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Rank all the hospital in the U.S."),
+  titlePanel("Rank all the hospitals in each state in the U.S."),
   sidebarLayout(
     sidebarPanel(
-      selectInput("disease", label= h3("mortality"),
+      selectInput("disease", label= h3("Outcome name"),
                   choices = c("heart attack",
                               "heart failure",
                               "pneumonia"),
                   selected = "heart attack"),
       radioButtons("rankBtn",
-                   label=h3("Rank of the hospitals in the U.S."),
+                   label=h3("Rank of the hospitals in each state"),
                    choices = list("best" = 1, 
                                   "worst" = 2, 
                                   "any number (n-th best hospital). You can
                                    set it in the input box below" = 3),
                    selected = 1),
-      numericInput("rankno", label="number", value=1)
+      numericInput("rankno", label="Number", value=3)
     
     ),
     mainPanel(tableOutput("result"))
